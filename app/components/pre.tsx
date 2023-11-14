b51beb7f849779aea337aea810160187d3bf9a89
Cuitao <ctllfh@gmail.com>
2023-11-14 18:42:27 +0800

    add copy button

--- app/components/pre.tsx
+++ app/components/pre.tsx
@@ -1,4 +1,5 @@
-import type { PropsWithChildren } from "react";
+import { useCallback, type PropsWithChildren } from "react";
+import { Button } from "./ui/button";
 
 type PreProps = PropsWithChildren<{ code: string; language?: string }>;
 
@@ -8,11 +9,20 @@ export default function Pre({
   language = "",
   ...other
 }: PreProps) {
-  console.log(language);
+  const copyHandler = useCallback(() => {
+    navigator.clipboard.writeText(code);
+  }, [code]);
   return (
     <pre {...other}>
-      <div className="border-b h-8 flex bg-muted items-center">
+      <div className="border-b h-8 flex bg-muted items-center justify-between">
         <span className="text-muted-foreground text-sm pl-4">{language}</span>
+        <Button
+          variant="link"
+          className="text-muted-foreground text-sm hover:hover:no-underline p-0 pr-4 h-fit"
+          onClick={copyHandler}
+        >
+          copy
+        </Button>
       </div>
       {children}
     </pre>
