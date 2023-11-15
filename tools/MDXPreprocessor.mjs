3ff2de1af8ed161f2abcd68fc5bcd7e9e3e52eca
cuitao <ctllfh@gmail.com>
2023-11-15 14:38:47 +0800

    add meta

--- tools/MDXPreprocessor.mjs
+++ tools/MDXPreprocessor.mjs
@@ -63,15 +63,34 @@ function estimateReadTime(text) {
 }
 
 /**
+ * @typedef {{
+ *  title: string,
+ *  date: string,
+ *  image: string,
+ *  slug: string,
+ *  estimatedReadTime: number,
+ *  description: string
+ *  }} PostSummary
+ */
+
+/**
  * An array of post objects.
  * @type {Array<object>}
  */
 const posts = mdxFiles.map((file) => {
   const mdxFile = fs.readFileSync(`${routesDirectory}/${file}`, "utf8");
-  const mdxHeader = mdxFile.match(mdxHeaderPattern)[1];
-  const mdxHeaderObject = parse(mdxHeader);
-  const postName = file.match(postNamePattern)[1];
+  const mdxHeaderStr = mdxFile.match(mdxHeaderPattern)[1];
+  const mdxHeaderObject = parse(mdxHeaderStr);
   let needRewrite = false;
+
+  /** @type {PostSummary} */
+  const postSummary = {};
+
+  postSummary.title =
+    mdxHeaderObject.meta.reduce(
+      (title, meta) => title || meta["title"],
+      null
+    ) || "Untitled";
 
   // If the date is not present in the MDX header, add it.
   if (!mdxHeaderObject.date) {
@@ -79,18 +98,7 @@ const posts = mdxFiles.map((file) => {
     needRewrite = true;
   }
 
-  // If the title is not present in the MDX header, add it.
-  if (!mdxHeaderObject.title) {
-    const title = mdxHeaderObject.meta.reduce((acc, cur) => {
-      if (!acc && cur.title) {
-        return cur.title;
-      } else {
-        return acc;
-      }
-    }, undefined);
-    mdxHeaderObject.title = title || mdxHeaderObject.title.replace(/"/g, '\\"');
-    needRewrite = true;
-  }
+  postSummary.date = mdxHeaderObject.date;
 
   // If the image is not present in the MDX header, add default image.
   if (!mdxHeaderObject.image) {
@@ -98,24 +106,71 @@ const posts = mdxFiles.map((file) => {
     needRewrite = true;
   }
 
+  postSummary.image = mdxHeaderObject.image;
+
+  postSummary.slug = file.match(postNamePattern)[1];
+
+  postSummary.estimatedReadTime = estimateReadTime(mdxFile);
+
+  postSummary.description =
+    mdxHeaderObject.meta.reduce(
+      (description, meta) =>
+        description || (meta["name"] === "description" && meta["content"]),
+      null
+    ) || "";
+
+  // If the twitter card is not presend in the MDX header, add twiter card meta
+  // and OG meta.
+  if (
+    !mdxHeaderObject.meta.reduce(
+      (hasTwitterCard, meta) =>
+        hasTwitterCard || meta["name"] === "twitter:card",
+      false
+    )
+  ) {
+    mdxHeaderObject.meta.push({
+      name: "twitter:card",
+      content: "summary_large_image",
+    });
+    mdxHeaderObject.meta.push({
+      name: "twitter:title",
+      content: postSummary.title,
+    });
+    mdxHeaderObject.meta.push({
+      name: "twitter:description",
+      content: postSummary.description,
+    });
+    mdxHeaderObject.meta.push({
+      property: "og:title",
+      content: postSummary.title,
+    });
+    mdxHeaderObject.meta.push({
+      property: "og:description",
+      content: postSummary.description,
+    });
+
+    if (postSummary.image !== "/images/blog_no_image_placeholder.png") {
+      mdxHeaderObject.meta.push({
+        name: "twitter:image",
+        content: mdxHeaderObject.image,
+      });
+      mdxHeaderObject.meta.push({
+        property: "og:image",
+        content: mdxHeaderObject.image,
+      });
+    }
+
+    needRewrite = true;
+  }
+
   // rewrite the MDX file if needed
   if (needRewrite) {
     const newMdxHeader = stringify(mdxHeaderObject);
-    const newMdxFile = mdxFile.replace(mdxHeader, newMdxHeader);
+    const newMdxFile = mdxFile.replace(mdxHeaderStr, newMdxHeader);
     fs.writeFileSync(`${routesDirectory}/${file}`, newMdxFile);
   }
 
-  return {
-    title: mdxHeaderObject.title,
-    date: mdxHeaderObject.date,
-    image: mdxHeaderObject.image,
-    slug: postName,
-    estimatedReadTime: estimateReadTime(mdxFile),
-    description: mdxHeaderObject.meta.reduce(
-      (acc, cur) => cur.description || acc,
-      ""
-    ),
-  };
+  return postSummary;
 });
 
 /**
